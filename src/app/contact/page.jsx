73c2bb4c5aee1657381a8e2029a16e1942d2f87b
rrrodzilla <rrrodzilla@protonmail.com>
2024-03-12 02:20:01 -0500

    fix:direct api call

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -50,7 +50,7 @@ const submitForm = async (formData) => {
     }
   }
   // Define the URL to post the data
-  const url = 'https://govcraft.ai/api/submit';
+  const url = 'https://ylw5nsj4f3.execute-api.us-east-1.amazonaws.com';
 
   try {
     // Send the POST request
